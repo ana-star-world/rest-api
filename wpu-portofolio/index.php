c7eab06eb6145def583f568368f56055295e6036
ana-star-world <rinahsb2003@gmail.com>
2025-05-31 22:01:02 +0700

    Memperbaharui Social Media

--- wpu-portofolio/index.php
+++ wpu-portofolio/index.php
@@ -1,3 +1,28 @@
+<?php
+function get_CURL($url)
+{
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec($curl);
+  curl_close($curl);
+
+  return json_decode($result, true);
+}
+
+$result = get_CURL('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCkXmLjEr95LVtGuIm3l2dPg&key=AIzaSyCrCxANmsYGyof2hn8DERaDFN-TVpfmCds');
+
+$youtubeProfilePic = $result ['items'][0]['snippet']['thumbnails']['medium']['url'];
+$channelName = $result ['items'][0]['snippet']['title'];
+$subscriber = $result ['items'][0]['statistics']['subscriberCount'];
+
+// latest video
+$urlLatestVideo = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyCrCxANmsYGyof2hn8DERaDFN-TVpfmCds&channelId=UCkXmLjEr95LVtGuIm3l2dPg&maxResults=1&order=date&part=snippet';
+$result = get_CURL($urlLatestVideo);
+$latestVideoId = $result['items'][0]['id']['videoId'];
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -17,7 +42,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#home">Sandhika Galih</a>
+        <a class="navbar-brand" href="#home">Rina Sopiana Hasibuan</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -31,6 +56,9 @@
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#portfolio">Portfolio</a>
+              <li class="nav-item">
+              <a class="nav-link" href="#portfolio"></a>
+            </li>
             </li>
           </ul>
         </div>
@@ -41,9 +69,9 @@
     <div class="jumbotron" id="home">
       <div class="container">
         <div class="text-center">
-          <img src="img/profile1.png" class="rounded-circle img-thumbnail">
-          <h1 class="display-4">Sandhika Galih</h1>
-          <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
+          <img src="img/Foto3.jpg" class="rounded-circle img-thumbnail">
+          <h1 class="display-4">Rina Sopiana Hasibuan</h1>
+          <h3 class="lead">Student | Gaming</h3>
         </div>
       </div>
     </div>
@@ -59,16 +87,22 @@
         </div>
         <div class="row justify-content-center">
           <div class="col-md-5">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, molestiae sunt doloribus error ullam expedita cumque blanditiis quas vero, qui, consectetur modi possimus. Consequuntur optio ad quae possimus, debitis earum.</p>
+            <p>Nama : Rina Sopiana Hasibuan</p>
+            <p>Ttl : Sibuhuan, Padang lawas, 11-Desember-2003</p>
+            <p>Hobby : Memasak & Main game</p>
+            <p>Warna favorit : hijau dan Biru</p>
           </div>
           <div class="col-md-5">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, molestiae sunt doloribus error ullam expedita cumque blanditiis quas vero, qui, consectetur modi possimus. Consequuntur optio ad quae possimus, debitis earum.</p>
+            <p>Saya Sekolah Dasar di SD 0128 Banjar Raja Sibuhuan, dan sekolah Menengah pertama dan ke atas saya di Pondok Pesantren Syekh Mhd. Dahlan Aek Hayuara Sibuhuan
+              Sekarang saya Kuliah di UIN Imam Bonjol Padang, Fakultas Sains dan Teknologi dengan Program Studi Sistem Informasi.</p>
           </div>
         </div>
       </div>
     </section>
 
-    <!--  Youtobe & IG  -->
+    <!--  Tiktok & YouTube  -->
+
+    <!-- Tiktok -->
     <section class="social bg-light" id="social">
       <div class="container">
         <div class="row pt-4 mb-4">
@@ -81,21 +115,54 @@
           <div class="col-md-5">
             <div class="row">
               <div class="col-md-4">
-                <img src="img/foto-Rina.jpg" width="130" class="img-thumbnail">
+                <img src="img/Foto1.jpg" width="300" class="rounded-circle img-thumbnail">
               </div>
             <div class="col-md-8">
-               <h5>rinahasibuan</h5>
+               <h5>Rina</h5>
+               <p>@rina.hsb11</p>
+               <p>96 Followers</p>
+            </div>
+          </div>
+          <div class="row mt-3 mb-3">
+            <div class="col">
+              <div style="max-height: 200px; overflow-y: auto;">
+              <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@rina.hsb11/video/7423749781308198149" data-video-id="7423749781308198149"  style="max-width: 80%; min-width: 150px;">
+              <section> </section>
+              </blockquote>
+              <script async src="https://www.tiktok.com/embed.js"></script>
+              </div>
             </div>
           </div>
           </div>
-          <div class="col-md-5"></div>
+
+          <!-- Youtube -->
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="<?= $youtubeProfilePic; ?>" width="120" class="rounded-circle img-thumbnail">
+              </div>
+               <div class="col-md-8">
+               <h5><?= $channelName; ?></h5>
+               <p><?= $subscriber; ?> Subscribers</p>
+              <div class="g-ytsubscribe" data-channelid="UCkXmLjEr95LVtGuIm3l2dPg" data-layout="default" data-count="default"></div>
+            </div>
+            </div>
+            <div class="row mt-3 pb-5">
+              <div class="col">
+                <iframe class="embed-responsive-item" 
+                src="https://www.youtube.com/embed/<?= $latestVideoId; ?>?rel=0" 
+                allowfullscreen></iframe>
+              </div>
+            </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
 
 
     <!-- Portfolio -->
-    <section class="portfolio bg-light" id="portfolio">
+    <section class="portfolio" id="portfolio">
       <div class="container">
         <div class="row pt-4 mb-4">
           <div class="col text-center">
@@ -183,9 +250,9 @@
             
             <ul class="list-group mb-4">
               <li class="list-group-item"><h3>Location</h3></li>
-              <li class="list-group-item">My Office</li>
-              <li class="list-group-item">Jl. Setiabudhi No. 193, Bandung</li>
-              <li class="list-group-item">West Java, Indonesia</li>
+              <li class="list-group-item">My Home</li>
+              <li class="list-group-item">Jl. Surapati, Link III, Banjar Raja, Sibuhuan</li>
+              <li class="list-group-item">North Sumatera, Indonesia</li>
             </ul>
           </div>
 
@@ -224,7 +291,7 @@
       <div class="container">
         <div class="row">
           <div class="col text-center">
-            <p>Copyright &copy; 2018.</p>
+            <p>Copyright &copy; 2025.</p>
           </div>
         </div>
       </div>
@@ -241,5 +308,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
   </body>
 </html>
